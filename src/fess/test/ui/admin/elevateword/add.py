a166cdfba9b3166e041c796c728fe07899b611ee
ytachiki <tachiki@n2sm.net>
2021-09-30 18:09:37 +0900

    add elevateword

---
+++ src/fess/test/ui/admin/elevateword/add.py
@@ -0,0 +1,54 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    context = browser.new_context()
+
+    # Open new page
+    page = context.new_page()
+
+    # Go to http://localhost:8080/login/
+    page.goto("http://localhost:8080/login/")
+
+    # Fill [placeholder="ユーザー名"]
+    page.fill("[placeholder=\"ユーザー名\"]", "admin")
+
+    # Fill [placeholder="パスワード"]
+    page.fill("[placeholder=\"パスワード\"]", "admin1234")
+
+    # Click button:has-text("ログイン")
+    page.click("button:has-text(\"ログイン\")")
+    # assert page.url == "http://localhost:8080/admin/dashboard/"
+
+    # Click text=サジェスト
+    page.click("text=サジェスト")
+
+    # Click text=追加ワード
+    page.click("text=追加ワード")
+    # assert page.url == "http://localhost:8080/admin/elevateword/"
+
+    # Click text=新規作成
+    page.click("text=新規作成")
+    # assert page.url == "http://localhost:8080/admin/elevateword/createnew/"
+
+    # Fill input[name="suggestWord"]
+    page.fill("input[name=\"suggestWord\"]", "application")
+
+    # Fill input[name="reading"]
+    page.fill("input[name=\"reading\"]", "app")
+
+    # Click button:has-text("作成")
+    page.click("button:has-text(\"作成\")")
+    # assert page.url == "http://localhost:8080/admin/elevateword/"
+
+    # Close page
+    page.close()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
