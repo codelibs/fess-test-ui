8d83c3449257b8ca2a31b95415c1ef0d0cd488d4
ytachiki <33273106+ytachiki@users.noreply.github.com>
2024-05-20 20:47:08 +0900

    add mapping (#10)
    * add mapping
    
    * fix mapping
    
    * update mapping

--- src/fess/test/ui/admin/dict/mapping/add.py
+++ src/fess/test/ui/admin/dict/mapping/add.py
@@ -1,58 +1,69 @@
+import logging
+import re
+
+from fess.test import assert_equal, assert_not_equal
+from fess.test.ui import FessContext
 from playwright.sync_api import Playwright, sync_playwright
 
+logger = logging.getLogger(__name__)
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    context = browser.new_context()
 
-    # Open new page
-    page = context.new_page()
+def setup(playwright: Playwright) -> FessContext:
+    context: FessContext = FessContext(playwright)
+    context.login()
+    return context
 
-    # Go to http://localhost:8080/login/
-    page.goto("http://localhost:8080/login/")
 
-    # Fill [placeholder="ユーザー名"]
-    page.fill("[placeholder=\"ユーザー名\"]", "admin")
+def destroy(context: FessContext) -> None:
+    context.close()
 
-    # Fill [placeholder="パスワード"]
-    page.fill("[placeholder=\"パスワード\"]", "admin1234")
 
-    # Click button:has-text("ログイン")
-    page.click("button:has-text(\"ログイン\")")
-    # assert page.url == "http://localhost:8080/admin/dashboard/"
+def run(context: FessContext) -> None:
+    logger.info(f"start")
+
+    page: "Page" = context.get_admin_page()
+    label_name: str = context.create_label_name()
 
     # Click text=システム
     page.click("text=システム")
 
     # Click text=辞書
     page.click("text=辞書")
-    # assert page.url == "http://localhost:8080/admin/dict/"
+    assert_equal(page.url, context.url("/admin/dict/"))
 
     # Click :nth-match(:text("mapping.txt"), 3)
     page.click(":nth-match(:text(\"mapping.txt\"), 3)")
-    # assert page.url == "http://localhost:8080/admin/dict/mapping/?dictId=bWFwcGluZy50eHQ="
+    assert_equal(page.url, context.url("/admin/dict/mapping/?dictId=bWFwcGluZy50eHQ="))
 
     # Click text=新規作成
     page.click("text=新規作成")
-    # assert page.url == "http://localhost:8080/admin/dict/mapping/createnew/bWFwcGluZy50eHQ%3D/"
+    assert_equal(page.url, context.url("/admin/dict/mapping/createnew/bWFwcGluZy50eHQ%3D/"))
 
     # Fill textarea[name="inputs"]
-    page.fill("textarea[name=\"inputs\"]", "一")
+    page.fill("textarea[name=\"inputs\"]", label_name)
 
     # Fill input[name="output"]
     page.fill("input[name=\"output\"]", "1")
 
     # Click button:has-text("作成")
     page.click("button:has-text(\"作成\")")
-    # assert page.url == "http://localhost:8080/admin/dict/mapping/list/1?dictId=bWFwcGluZy50eHQ="
+    assert_equal(page.url, context.url("/admin/dict/mapping/list/1?dictId=bWFwcGluZy50eHQ="))
 
-    # Close page
-    page.close()
+    page.wait_for_load_state("domcontentloaded")
 
-    # ---------------------
-    context.close()
-    browser.close()
+    # Go to last page
+    page_info: str = page.inner_text("div.col-sm-2")
+    last_page = int(re.search(r'(\d+)/(\d+)', page_info).group(2)) if re.search(r'(\d+)/(\d+)', page_info) else None
+    page.goto(page.url.replace("/list/1", f"/list/{last_page}"))
+    
+    page.wait_for_load_state("domcontentloaded")
+    table_content: str = page.inner_text("table")
+    assert_not_equal(table_content.find(label_name), -1,
+                     f"{label_name} not in {table_content}")
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        context: FessContext = setup(playwright)
+        run(context)
+        destroy(context)
